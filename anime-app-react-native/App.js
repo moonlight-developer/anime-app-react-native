bc4547014771e03a7760a3359bb80fc7cc214928
AbdulhamitSahadi <sahadi.abdulhamit@gmail.com>
2024-05-17 01:03:48 +0300

    UpdateingAppFunction

--- anime-app-react-native/App.js
+++ anime-app-react-native/App.js
@@ -1,32 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './src/navigation';
-import {initializeApp} from '@firebase/app'
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signout} from '@firebase/auth'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useFonts } from "expo-font";
+import AppLoading from 'expo-app-loading';
+import Home from './screens/Home';
+import Animedetails from './screens/Animedetails';
+import Episodewatch from './screens/Episodewatch';
+import Search from './screens/Search';
+import Watchlist from './screens/Watchlist';
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD6xjQAh46U2Y9uIcA_oEFANBBO3WxuQEI",
-  authDomain: "anime-app-19cf5.firebaseapp.com",
-  projectId: "anime-app-19cf5",
-  storageBucket: "anime-app-19cf5.appspot.com",
-  messagingSenderId: "382104175527",
-  appId: "1:382104175527:web:e3f90bf53c90251efeac82",
-  measurementId: "G-0THWG22FV3"
-};
+const Stack = createNativeStackNavigator();
 
-const app = initializeApp(firebaseConfig);
+export default function App() {
 
-const queryClient = new QueryClient();
+  const [fontsLoaded] = useFonts({
+    'pop-bold': require("./assets/fonts/Poppins-Bold.ttf"),
+    'pop-medium': require("./assets/fonts/Poppins-Medium.ttf"),
+    'pop-regular': require("./assets/fonts/Poppins-Regular.ttf"),
+    'lob-bold': require("./assets/fonts/LobsterTwo-Bold.ttf"),
+    'lob-regular': require("./assets/fonts/LobsterTwo-Regular.ttf"),
+  })
 
-export default function App(email, setEmail, setPassword, isLogin, setIsLogin, handleAuthentication) {
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppNavigation />
-    </QueryClientProvider>
+    <GestureHandlerRootView style={styles.container}>
+    <StatusBar style="light" />
+    <View style={styles.container}>                 
+      <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen  name='Home' component={Home} options={{ headerShown: false }} />
+            <Stack.Screen  name='Animedetails' component={Animedetails} options={{ headerShown: false }}  />
+            <Stack.Screen  name='Episodewatch' component={Episodewatch} options={{ headerShown: false }}  />
+            <Stack.Screen  name='Search' component={Search} options={{ headerShown: false }}  />
+            <Stack.Screen  name='Watchlist' component={Watchlist} options={{ headerShown: false }}  />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
